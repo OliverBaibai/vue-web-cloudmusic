230337540dac6303d79a6664ef4f083bd124588c
熙穆 <baij@xiansuoer.com>
2020-09-11 18:56:29 +0800

    gotop

--- src/components/common/gotop/Index.vue
+++ src/components/common/gotop/Index.vue
@@ -1,11 +1,94 @@
 <template>
-  <div class="back-to-top">
+  <div
+    class="back-to-top"
+    @click="backToTop"
+    v-show="showReturnToTop"
+    @mouseenter="show"
+    @mouseout="hide"
+  >
     <img src="../../../assets/images/gotop.png" />
   </div>
 </template>
 
 <script>
-export default {};
+import { scrollIt } from "@/common/scrollIt";
+export default {
+  name: "back-to-top",
+  data() {
+    return {
+      showTooltips: false,
+      showReturnToTop: false
+    };
+  },
+  props: {
+    // 文本提示
+    text: {
+      type: String,
+      default: "返回顶部"
+    },
+    textColor: {
+      // 文本颜色
+      type: String,
+      default: "#f00"
+    },
+    iPos: {
+      // 文本位置
+      type: String,
+      default: "right"
+    },
+    iClass: {
+      // 图标形状
+      type: String,
+      default: "fzicon fz-ad-fanhuidingbu1"
+    },
+    iColor: {
+      // 图标颜色
+      type: String,
+      default: "#f00"
+    },
+    iFontsize: {
+      // 图标大小
+      type: String,
+      default: "32px"
+    },
+    pageY: {
+      // 默认在哪个视图显示返回按钮
+      type: Number,
+      default: 200
+    },
+    transitionName: {
+      // 过渡动画名称
+      type: String,
+      default: "linear"
+    }
+  },
+  components: {},
+  computed: {},
+  watch: {},
+  methods: {
+    show() {
+      retrun((this.showTooltips = true));
+    },
+    hide() {
+      return (this.showTooltips = false);
+    },
+    currentPageYOffset() {
+      // 判断滚动区域大于多少的时候显示返回顶部的按钮
+      window.pageYOffset > this.pageY
+        ? (this.showReturnToTop = true)
+        : (this.showReturnToTop = false);
+    },
+    backToTop() {
+      scrollIt(0, 500, this.transitionName, this.currentPageYOffset);
+    }
+  },
+  created() {
+    window.addEventListener("scroll", this.currentPageYOffset, true);
+  },
+  beforeDestroy() {
+    window.removeEventListener("scroll", this.currentPageYOffset, true);
+  }
+};
 </script>
 
 
--- src/model/video.js
+++ src/model/video.js
@@ -3,14 +3,22 @@
 import utils from '../utils/utils'
 
 export default class Video {
-  constructor({ id, nickName, name, playCount, duration, image, isLive }) {
-    ;(this.id = id),
-      (this.nickName = nickName),
-      (this.name = name),
-      (this.playCount = playCount),
-      (this.duration = duration),
-      (this.image = image),
-      (this.isLive = isLive)
+  constructor({
+    id,
+    nickName,
+    name,
+    playCount,
+    duration,
+    image,
+    isLive
+  }) {
+    (this.id = id),
+    (this.nickName = nickName),
+    (this.name = name),
+    (this.playCount = playCount),
+    (this.duration = duration),
+    (this.image = image),
+    (this.isLive = isLive)
   }
 }
 
